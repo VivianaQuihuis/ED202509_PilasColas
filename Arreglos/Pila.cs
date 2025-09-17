c4cdc97e3c42f86d51acf39671b09a530aaaa0d8
Yunibe <yunipandy732@gmail.com>
2025-09-17 08:59:14 -0600

    Empezando a trabajar con pila

---
+++ Arreglos/Pila.cs
@@ -0,0 +1,23 @@
+﻿namespace Arreglos
+{
+    internal class Pila
+    {
+        string[] _arreglo;
+        int _tope;
+
+        public Pila(int elementos)
+        {
+            _arreglo = new string[elementos];
+            _tope = 0;
+        }
+
+        public void Agregar() 
+        {
+            if(_tope > _arreglo.Length) 
+            {
+
+            }
+        }
+
+    }
+}
