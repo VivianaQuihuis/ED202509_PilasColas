de62fd5f256f718d91f606e85944ab459c34459c
VivianaQuihuis <viviana_quihuis24@unikino@edu.mx>
2025-10-21 09:43:56 -0700

    Proyecto Parcial

--- Listas/Cola.cs
+++ Listas/Cola.cs
@@ -8,5 +8,48 @@ namespace Listas
 {
     internal class Cola
     {
+        int _inicio;
+        int _final;
+        List <string> _arreglo;
+
+        public Cola(int elementos)
+        {
+            _inicio = 0;
+            _final = 0;
+            _arreglo = new List<string>(new string[elementos]);
+        }
+
+        public void Agregar(string dato)
+        {
+            int max = _arreglo.Count - 1;
+            if (_final > max)
+            {
+                throw new Exception("Cola llena");
+            }
+            _arreglo[_final] = dato;
+            _final++;
+        }
+
+        public void Eliminar()
+        {
+            if (_final == 0 || _inicio == _final)
+            {
+                throw new Exception("Cola vacia");
+            }
+
+            _arreglo[_inicio] = string.Empty;
+            _inicio++;
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+            for (int i = _inicio; i < _final; i++)
+            {
+                datos += _arreglo[i] + Environment.NewLine;
+            }
+            return datos;
+        }
     }
 }
+
--- Listas/Pila.cs
+++ Listas/Pila.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Listas
+﻿namespace Listas
 {
     internal class Pila
     {
+        List<string> _arreglo;
+
+        public Pila(int elementos)
+        {
+            _arreglo = new List<string>(elementos);
+        }
+
+        public void Agregar(string dato)
+        {
+            if (_arreglo.Count >= _arreglo.Capacity) ;
+            {
+                throw new Exception("No hay espacio");
+            }
+
+            _arreglo.Add(dato);
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+            for (int i = 0; i < _arreglo.Count; i++)
+            {
+                string dato = _arreglo[i];
+                datos += $"{dato}{Environment.NewLine}";
+            }
+            return datos;
+        }
+
+        public void Eliminar()
+        {
+            if (_arreglo.Count <= 0)
+            {
+                throw new Exception("No hay elementos");
+            }
+            _arreglo.RemoveAt(_arreglo.Count - 1);
+        }
     }
 }
